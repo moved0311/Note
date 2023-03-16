50264fcaccc266d9080b51524b8eb2ba5d9c6703
taiyi <taiyijiang@anuegroup.com.tw>
2023-03-16 18:30:10 +0800

    note backup: 2023-03-16 18:30:10

--- LeetCode/260.single-number-iii.ts
+++ LeetCode/260.single-number-iii.ts
@@ -6,7 +6,20 @@
 
 // @lc code=start
 function singleNumber(nums: number[]): number[] {
+  const result = [1, 2, 3];
 
-};
+  return result;
+}
 // @lc code=end
 
+console.log(singleNumber([1, 2, 1, 3, 2, 5]));
+
+/*
+ 1. Time: O(n), Space: O(n) 
+  const dict: { [key: string]: number } = {};
+  nums.map((n) => (dict[n] = (dict[n] || 0) + 1));
+
+  const result = Object.keys(dict)
+    .filter((key) => dict[key] === 1)
+    .map((v) => parseInt(v));
+*/
