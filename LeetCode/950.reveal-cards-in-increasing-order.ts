585a44235722b65f48362de5487e507786dccd7b
taiyi <taiyijiang@anuegroup.com.tw>
2023-04-13 18:08:48 +0800

    note backup: 2023-04-13 18:08:48

---
+++ LeetCode/950.reveal-cards-in-increasing-order.ts
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=950 lang=typescript
+ *
+ * [950] Reveal Cards In Increasing Order
+ */
+
+// @lc code=start
+function deckRevealedIncreasing(deck: number[]): number[] {
+  const result = [...deck.sort((a, b) => b - a)];
+
+  return result.reduce((arr: number[], num: number) => {
+    if (arr.length >= 2) {
+      let last = arr.pop();
+
+      if (last) arr.unshift(last);
+    }
+
+    arr.unshift(num);
+
+    return arr;
+  }, []);
+}
+
+// @lc code=end
+
+console.log(deckRevealedIncreasing([17, 13, 11, 2, 3, 5, 7]));
+
+/**
+ * [2,13,3,11,5,17,7]
+ * 2 [3,11,5,17,7,13]
+ * 3 [5,17,7,13,11]
+ * 5 [7,13,11,17]
+ * 7 [11,17,13]
+ * 11 [13,17]
+ * 13 [17]
+ * 17 []
+ */
